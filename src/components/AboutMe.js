f5c8fc9eda5f9e458e5ef649036a5502c8bf8b50
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-14 20:05:34 -0400

    rejigger about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,33 +6,29 @@ import '../stylesheets/AboutMe.css'
 const AboutMe = () => {
   return (
     <section className='cards cards-container' id='me-container'>
-      <div className='work'>
-        <img src={linkedin2} className='my-photo' alt='my-photo' />
-        <h3>
-          I've always had a penchant for creativity. My life has been a
-          continual exploration of mediums ranging from textiles and
-          metalsmithing to designing for editorial publications, both print and
-          digital. The common thread throughout these mediums is the fascination
-          of making the abstract tangible. <br /> <br />
-          As a Software Engineer, my focus is on exploring this medium of
-          computer programming - applying my creative and technical
-          problem-solving skills, as well as my tenacious spirit. Because of my
-          previous work environment, I'm adept at prioritizing key deliverables
-          under tight deadlines and collaborating effectively across
-          departments.
-          <br /> <br />
-          <div>
-            <a href={cynthiaeddy_resume}>
-              <h5>view my resume | </h5>
-            </a>
-            <a href='http://www.cynthiaeddy.com'>
-              <h5>view my print and digital portfolio | </h5>
-            </a>
-            <a href={cynthiaeddy_resume} download>
-              <h5> download my resume</h5>
-            </a>
-          </div>
-        </h3>
+      <img src={linkedin2} className='my-photo' alt='my-photo' />
+      <h3>
+        I've always had a penchant for creativity. My life has been a continual
+        exploration of mediums ranging from textiles and metalsmithing to
+        designing for editorial publications, both print and digital. The common
+        thread throughout these mediums is the fascination of making the
+        abstract tangible. <br /> <br />
+        As a Software Engineer, my focus is on exploring this medium of computer
+        programming - applying my creative and technical problem-solving skills,
+        as well as my tenacious spirit. Because of my previous work environment,
+        I'm adept at prioritizing key deliverables under tight deadlines and
+        collaborating effectively across departments.
+      </h3>
+      <div className='my-links'>
+        <a href={cynthiaeddy_resume}>
+          <h5>view my resume | </h5>
+        </a>
+        <a href='http://www.cynthiaeddy.com'>
+          <h5>view my print and digital portfolio | </h5>
+        </a>
+        <a href={cynthiaeddy_resume} download>
+          <h5> download my resume</h5>
+        </a>
       </div>
     </section>
   )
