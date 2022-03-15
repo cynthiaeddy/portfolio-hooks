bb99e6bc4bd04846e1016bf429074ae5a4674818
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-14 20:51:33 -0400

    restyle about me

--- src/components/stash/AboutMe.js
+++ src/components/stash/AboutMe.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import linkedin2 from '../assets/linkedin2.jpg'
 import cynthiaeddy_resume from '../assets/cynthiaeddy_resume.pdf'
 import '../stylesheets/Projects.css'
@@ -6,21 +5,21 @@ import '../stylesheets/AboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div className='cards cards-container' id='me-container'>
-      <img src={linkedin2} className='my-photo' alt='' />
+    <section className=' cards cards-container' id='me-container'>
+      <img src={linkedin2} className='my-photo' alt='my-photo' />
       <h3>
         I've always had a penchant for creativity. My life has been a continual
         exploration of mediums ranging from textiles and metalsmithing to
         designing for editorial publications, both print and digital. The common
         thread throughout these mediums is the fascination of making the
         abstract tangible. <br /> <br />
-        As a Software Engineer, my focus is on exploring this medium of computer
-        programming - applying my creative and technical problem-solving skills,
-        as well as my tenacious spirit. Because of my previous work environment,
-        I'm adept at prioritizing key deliverables under tight deadlines and
-        collaborating effectively across departments.
+        As a Software Developer, my focus is on exploring this medium of
+        computer programming - applying my creative and technical
+        problem-solving skills, as well as my tenacious spirit. Because of my
+        previous work environment, I'm adept at prioritizing key deliverables
+        under tight deadlines and collaborating effectively across departments.
       </h3>
-      <footer>
+      <div className='my-links'>
         <a href={cynthiaeddy_resume}>
           <h5>view my resume | </h5>
         </a>
@@ -30,8 +29,8 @@ const AboutMe = () => {
         <a href={cynthiaeddy_resume} download>
           <h5> download my resume</h5>
         </a>
-      </footer>
-    </div>
+      </div>
+    </section>
   )
 }
 
